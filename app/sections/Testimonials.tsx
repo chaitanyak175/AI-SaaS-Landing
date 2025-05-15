e642d667804c4907704db8670929b639d7448048
chaitanyak175 <karmalkarchaitanya@gmail.com>
2025-05-15 18:28:45 +0530

    fixed the linting issue

--- app/sections/Testimonials.tsx
+++ app/sections/Testimonials.tsx
@@ -42,8 +42,8 @@ export const Testimonials = () => {
                     Beyond Expectations.
                 </h2>
                 <p className="text-white/70 text-lg md:text-xl text-center mt-5 tracking-tight max-w-sm mx-auto">
-                    Our revoultionary AI SEO tools have transfromed our client's
-                    stratergies.
+                    Our revoultionary AI SEO tools have transfromed our
+                    client&apos;s stratergies.
                 </p>
                 <div className="flex overflow-hidden mt-10 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
                     <motion.div
