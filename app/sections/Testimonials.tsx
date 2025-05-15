b4eb2c29cc1889c24615f39144d3afdec705f251
chaitanyak175 <karmalkarchaitanya@gmail.com>
2025-05-15 18:39:33 +0530

    trying a new fix

--- app/sections/Testimonials.tsx
+++ app/sections/Testimonials.tsx
@@ -43,7 +43,7 @@ export const Testimonials = () => {
                 </h2>
                 <p className="text-white/70 text-lg md:text-xl text-center mt-5 tracking-tight max-w-sm mx-auto">
                     Our revoultionary AI SEO tools have transfromed our
-                    client&apos;s stratergies.
+                    client&apos;s strategies.
                 </p>
                 <div className="flex overflow-hidden mt-10 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
                     <motion.div
